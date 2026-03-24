afc1850c93a1356f898a368ace7642c9d599c1a5
baldiii <up202405858@edu.fe.up.pt>
2026-03-24 16:49:18 +0000

    ups

--- include/cli_parser.hpp
+++ include/cli_parser.hpp
@@ -1,12 +1,15 @@
 #pragma once
-
+using namespace std;
+#include "node_data.hpp"
 #include <string>
+#include <vector>
 
 // parser for batch mode execution
 int parseArguments(int argc, char *argv[]);
 
 // parser for menu mode execution
-int parseInput(std::string);
+int parseInput(string inputFile, vector<DataNode>& allNodes,Parameters &params, Control&ctrl);
+
 
 // --- helper methods ---
 bool hasCsvExtension(const std::string &filename);
